81b42661c833b697ce34fdb5859718e34afb71e0
Mohammed Faraz <mdfaraz@sparkgenieit.com>
2025-01-04 13:27:20 +0530

    frontend

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const request = axios.create({
-    baseURL: "http://localhost:8000/api/v1/",
+    baseURL: "https://sales-management-system-server.onrender.com/api/v1/",
     headers: {
         'Content-Type': 'application/json',
     },
